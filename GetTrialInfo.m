c3f38fca65f2d5396eb0e4f7d1d72a3b0bae033f
JoshBarrios <jbarrios.home@gmail.com>
2019-02-19 21:20:04 -0700

    Fixed GetTrialInfo to deal with numbers with more leading zeros

--- GetTrialInfo.m
+++ GetTrialInfo.m
@@ -10,22 +10,16 @@ if contains(subPath,'Fish')
     fishInd = strfind(subPath,'Fish');
     if fishInd + 5 > length(subPath)
         fishNum = str2num(subPath(fishInd + 4));
-    else if isempty(str2num(subPath(fishInd + 5)))
-            fishNum = str2num(subPath(fishInd + 4));
-        else
-            fishNum = str2num(subPath(fishInd + 4: fishInd + 5));
-        end
+    else 
+        fishNum = findNum(subPath(fishInd + 4:end));
     end
     
 else contains(subPath,'F');
     fishInd = strfind(subPath,'F');
     if fishInd + 2 > length(subPath)
         fishNum = str2num(subPath(fishInd + 1));
-    else if isempty(str2num(subPath(fishInd + 2)))
-            fishNum = str2num(subPath(fishInd + 1));
-        else
-            fishNum = str2num(subPath(fishInd + 1: fishInd + 2));
-        end
+    else 
+        fishNum = findNum(subPath(fishInd + 1:end));
     end
 end
 
@@ -34,22 +28,16 @@ if contains(subPath,'Plane')
     planeInd = strfind(subPath,'Plane');
     if planeInd + 6 > length(subPath)
         planeNum = str2num(subPath(planeInd + 5));
-    else if isempty(str2num(subPath(planeInd + 6)))
-            planeNum = str2num(subPath(planeInd + 5));
-        else
-            planeNum = str2num(subPath(planeInd + 5: planeInd + 6));
-        end
+    else 
+        planeNum = findNum(subPath(planeInd + 5:end));
     end
     
 else contains(subPath,'P');
     planeInd = strfind(subPath,'P');
     if planeInd + 2 > length(subPath)
         planeNum = str2num(subPath(planeInd + 1));
-    else if isempty(str2num(subPath(planeInd + 2)))
-            planeNum = str2num(subPath(planeInd + 1));
-        else
-            planeNum = str2num(subPath(planeInd + 1: planeInd + 2));
-        end
+    else 
+        planeNum = findNum(subPath(planeInd + 1:end));
     end
 end
 
@@ -58,22 +46,16 @@ if contains(subPath,'Trial')
     trialInd = strfind(subPath,'Trial');
     if trialInd + 6 > length(subPath)
         trialNum = str2num(subPath(trialInd + 5));
-    else if isempty(str2num(subPath(trialInd + 6)))
-            trialNum = str2num(subPath(trialInd + 5));
-        else
-            trialNum = str2num(subPath(trialInd + 5: trialInd + 6));
-        end
+    else 
+        trialNum = findNum(subPath(trialInd + 5:end));
     end
     
 else contains(subPath,'T');
     trialInd = strfind(subPath,'T');
     if trialInd + 2 > length(subPath)
         trialNum = str2num(subPath(trialInd + 1));
-    else if isempty(str2num(subPath(trialInd + 2)))
-            trialNum = str2num(subPath(trialInd + 1));
-        else
-            trialNum = str2num(subPath(trialInd + 1: trialInd + 2));
-        end
+    else 
+        trialNum = findNum(subPath(trialInd + 1:end));
     end
 end
 
---
+++ findNum.m
@@ -0,0 +1,16 @@
+%% Josh Barrios 2/19/19
+% Takes a string and an index value and looks for a number following that
+% index. Intended to find fish numbers and trial numbers in filenames.
+% Start the string with the index after 'Fish' or 'F' and go to end.
+
+function num = findNum(string)
+
+
+
+for k = 1:length(string)
+    numBool(k) = ~isnan(str2double(string(k)));
+end
+
+seqs = findseq(double(numBool));
+
+num = str2double(string(seqs(1,2):seqs(1,3)));
