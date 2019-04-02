5dc4cb3428f64ad67f5c4ecbd04074c2c8eadfbb
JoshBarrios <jbarrios.home@gmail.com>
2019-04-02 14:23:58 -0600

    removed use of reserved word path

--- ReadDirFunc.m
+++ ReadDirFunc.m
@@ -1,9 +1,9 @@
-function pictures = ReadDirFunc(path)
+function pictures = ReadDirFunc(parentPath)
 % path = uigetdir;
 tic;
 display('=====Reading in directory=====');
 
-d = dir(path);
+d = dir(parentPath);
 
 for k = 1:length(d)
     if contains(d(k).name,'.tif')
@@ -17,14 +17,14 @@ imInds = find(isIm);
 
 num_images = length(imInds);
     
-pictures(:,:,1) = imread(fullfile(path,d(imInds(1)).name));
+pictures(:,:,1) = imread(fullfile(parentPath,d(imInds(1)).name));
 [picx,picy] = size(pictures(:,:,1));
 pictures = zeros(picx,picy,num_images);
 
 parfor m = 1:num_images
     ind = imInds(m);
         imname = d(ind).name;
-        fullname = fullfile(path,imname);
+        fullname = fullfile(parentPath,imname);
         pictures(:,:,m) = imread(fullname);
 end    
 
